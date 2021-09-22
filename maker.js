08735fc94fbf7b730cc2e52f67107002edf8b356
Azhaziq Goh <haziqgoh23@gmail.com>
2021-09-23 01:37:05 +0800

    full solution

--- maker.js
+++ maker.js
@@ -1,1 +1,34 @@
-var Mustache = require('mustache');
+const fs = require('fs');
+const Mustache = require('mustache');
+const json = require('./config.json');
+
+let counter = 1;
+let data = { ...json };
+
+data.style = function () {
+  let bgColor = "";
+  if(counter%2 === 0) {
+    bgColor = "background-color: rgba(76, 76, 76, 0.02)"
+  } else {
+    bgColor = "background-color: rgba(76, 76, 76, 0.1)";
+  }
+
+  counter++;
+  return bgColor;
+}
+
+let rendered = "";
+
+try {
+  const template = fs.readFileSync('./template.mustache','utf8');
+  rendered = Mustache.render(template, data);
+} catch (err) {
+  console.log(err);
+}
+
+try {
+  fs.writeFileSync('./result.html', rendered);
+  //file written successfully
+} catch (err) {
+  console.error(err)
+}
